7ef28dccf99822dfb8e96b32cd74acf8cf402ca2
BILLAHMASUM325 <masumbillah3.25@outlook.com>
2021-12-10 00:01:00 +0600

    javascript-practice

--- fibonacciRecursive.js
+++ fibonacciRecursive.js
@@ -1,13 +1,31 @@
-function fiboncci(n){
+// function fiboncci(n){
+//     if(n==0){
+//         return 0;
+//     }
+//     if(n==1){
+//         return 1;
+//     }
+//     else{
+//         return fiboncci(n-1)+fiboncci(n-2);
+//     }
+// }
+// var result = fiboncci(10)
+// console.log(result)
+
+
+function fibonacci(n){
     if(n==0){
-        return 0;
+        return[0];
     }
-    if(n==1){
-        return 1;
+    else if(n==1){
+        return[0,1];
     }
     else{
-        return fiboncci(n-1)+fiboncci(n-2);
+        var fibo = fibonacci(n-1);
+        var nextElement = fibo[n-1]+fibo[n-2];
+        fibo.push(nextElement);
+        return fibo;
     }
 }
-var result = fiboncci(10)
-console.log(result)
+var result = fibonacci(10);
+console.log(result); 
